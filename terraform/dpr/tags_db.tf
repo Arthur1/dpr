64cf00eb1e263db68a4ea8fb5a1df4f46a0b0dfd
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2024-02-24 06:39:25 +0900

    add GSI

--- terraform/dpr/tags_db.tf
+++ terraform/dpr/tags_db.tf
@@ -4,10 +4,23 @@ resource "aws_dynamodb_table" "tags_db" {
   read_capacity  = 3
   write_capacity = 3
 
-  hash_key = "tag"
   attribute {
     name = "tag"
     type = "S"
+  }
+  attribute {
+    name = "object_key"
+    type = "S"
+  }
+  hash_key = "tag"
+
+  global_secondary_index {
+    name            = "index_object_key_tag"
+    hash_key        = "object_key"
+    range_key       = "tag"
+    projection_type = "ALL"
+    read_capacity   = 3
+    write_capacity  = 3
   }
 
   tags = var.tags
