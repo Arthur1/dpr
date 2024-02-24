8ea7e9df06f17bd239473c4d88380861dc802c5a
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2024-02-25 06:10:13 +0900

    remove file hash

--- terraform/dpr/modules/scheduled_cleaner/lambda.tf
+++ terraform/dpr/modules/scheduled_cleaner/lambda.tf
@@ -1,16 +1,15 @@
 
 resource "aws_lambda_function" "scheduled_cleaner" {
-  function_name    = var.function_name
-  runtime          = "provided.al2023"
-  handler          = "main"
-  role             = aws_iam_role.scheduled_cleaner.arn
-  architectures    = ["arm64"]
-  memory_size      = var.function_memory_size
-  timeout          = var.function_timeout
-  package_type     = "Zip"
-  filename         = "./dpr-cleaner-eventbridge-lambda.zip"
-  source_code_hash = filebase64sha256("./dpr-cleaner-eventbridge-lambda.zip")
-  tags             = var.tags
+  function_name = var.function_name
+  runtime       = "provided.al2023"
+  handler       = "main"
+  role          = aws_iam_role.scheduled_cleaner.arn
+  architectures = ["arm64"]
+  memory_size   = var.function_memory_size
+  timeout       = var.function_timeout
+  package_type  = "Zip"
+  filename      = "./dpr-cleaner-eventbridge-lambda.zip"
+  tags          = var.tags
   depends_on = [
     terraform_data.dpr_cleaner_eventbridge_lambda
   ]
