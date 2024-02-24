be1e54089f96e72072590de7321755ff0d8991d3
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2024-02-25 05:59:03 +0900

    fix variable path

--- terraform/dpr/scheduled_cleaner.tf
+++ terraform/dpr/scheduled_cleaner.tf
@@ -1,7 +1,7 @@
 module "scheduled_cleaner" {
   source                     = "./modules/scheduled_cleaner"
-  package_store_s3_bucket_id = module.aws_s3_bucket.package_store.id
-  tag_db_dynamodb_table_id   = module.aws_dynamodb_table.tag_db.id
+  package_store_s3_bucket_id = aws_s3_bucket.package_store.id
+  tag_db_dynamodb_table_id   = aws_dynamodb_table.tag_db.id
   function_name              = "dpr-scheduled-cleaner"
   rule_schedule_expression   = "rate(5 min)"
   tags                       = var.tags
